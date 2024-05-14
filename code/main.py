a14910ff9cc0479be6ae44e0f0e7aca228b04375
Jianan (Landy) Zhuo <121222373+landyzhuo@users.noreply.github.com>
2024-05-15 01:03:18 +0800

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,1 +1,13 @@
+import nltk
 
+# splits words and punctuations up and store into array
+def tokenize():
+    pass
+
+# identify word roots and store into array
+def stem():
+    pass
+
+# eliminate punctuations from array
+def bagOfWords():
+    pass
