a678799e8a2c008c2251e059f08cc225c9df78e0
Jianan (Landy) Zhuo <121222373+landyzhuo@users.noreply.github.com>
2024-05-15 01:05:02 +0800

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,13 +1,13 @@
 import nltk
 
 # splits words and punctuations up and store into array
-def tokenize():
+def tokenize(prompt):
     pass
 
 # identify word roots and store into array
-def stem():
+def stem(word):
     pass
 
 # eliminate punctuations from array
-def bagOfWords():
+def bagOfWords(tokenized_prompt, word_list):
     pass
