1ede127b33de04802b5ee88dab82dab404ab8934
Jianan (Landy) Zhuo <landy_zhuo@outlook.com>
2024-05-16 00:03:53 +0800

    Adjusted 'utils.py' and 'README.md'

--- code/utils.py
+++ code/utils.py
@@ -1,12 +1,20 @@
+# imports NLTK library
 import nltk
+# imports the Porter Stemmer
+from nltk.stem.porter import PorterStemmer
+
+# pre-trained AI tokenizer
+nltk.download("punkt")
+# define your stemmer
+stemmer = PorterStemmer()
 
 # splits words and punctuations up and store into array
 def tokenize(prompt):
-    pass
+    return nltk.word_tokenize(prompt)
 
 # identify word roots and store into array
 def stem(word):
-    pass
+    return stemmer.stem(word.lower())
 
 # eliminate punctuations from array
 def bagOfWords(tokenized_prompt, word_list):
