7398ddf21503e02be989243dcaf45ea77e1db150
Jianan (Landy) Zhuo <landy_zhuo@outlook.com>
2024-05-16 23:23:50 +0800

    Adjusted train.py

--- code/train.py
+++ code/train.py
@@ -1,6 +1,23 @@
 import json
+from utils import tokenize, stem, bagOfWords
 
 with open('intents.json','r') as f:
     intents = json.load(f)
 
-print(intents)
+words = []
+tags = []
+xy = []
+
+for intent in intents['intents']:
+    tag = intent['tag']
+    tags.append(tag)
+    for pattern in intent['patterns']:
+        w = tokenize(pattern)
+        words.extend(w)
+        xy.append((w, tag))
+
+exclude_symbols = ['?','!','.',',',':',';','"',"'","-","/"]
+words = [stem(i) for i in words if i not in exclude_symbols]
+words = sorted(set(words))
+tags = sorted(set(tags))
+print(tags, words)
